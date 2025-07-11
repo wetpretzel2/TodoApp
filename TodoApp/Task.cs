8510aaf68bc8374adf432a4a4ce18b053f2e4490
scott <scott.dare.sd@gmail.com>
2025-07-11 15:47:44 +1000

    No comment required.

---
+++ TodoApp/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public abstract class Task
+    {
+        public string TaskTitle { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public abstract void Display();
+
+    }
+}
