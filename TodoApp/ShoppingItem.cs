17585da802bc8df2e0b82af0d6bdec64ebd635e1
scott <scott.dare.sd@gmail.com>
2025-07-13 18:40:54 +1000

    Added the ShoppingItem.cs

---
+++ TodoApp/ShoppingItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TodoApp
+{
+    internal class ShoppingItem : Task
+    {
+        public int Quantity { get; set; }
+
+        public ShoppingItem(string title, DateTime dueDate, int quantity): base(title, dueDate)
+        {
+            Quantity = quantity;
+        }
+        public override void Display()
+        {
+            Console.WriteLine($"Shopping Item: {TaskTitle}, Due: {DueDate}, Quantity: {Quantity}");
+
+        }
+    }
+}
